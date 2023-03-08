b04dfd67dc2a33bc8755acd378a93de2a1077695
opas-keat <opas.keat@gmail.com>
2023-03-08 21:22:09 +0700

    init

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'app/routes/app_pages.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      defaultTransition: Transition.noTransition,
+      title: "PPSW DEALER",
+      // localizationsDelegates: GlobalMaterialLocalizations.delegates,
+      // supportedLocales: const [
+      //   Locale('th', 'TH'),
+      // ],
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
+      // unknownRoute: GetPage(
+      //   name: "/not-found",
+      //   page: () => const PageNotFound(),
+      //   transition: Transition.noTransition,
+      // ),
+      theme: ThemeData(
+        useMaterial3: false,
+        textTheme: GoogleFonts.sarabunTextTheme(),
+        // primaryColor: primaryColor,
+        // primaryColorDark: primaryLightColor,
+        // colorScheme: ColorScheme.fromSwatch().copyWith(
+        //   secondary: accentColor,
+        // ),
+        // appBarTheme: const AppBarTheme(
+        //   iconTheme: IconThemeData(color: Colors.black),
+        //   color: primaryColor,
+        // ),
+      ),
+    );
+  }
+}

cd1575c783c6e9370f94d9b2c2f77f65c1a921d0
opas-keat <opas.keat@gmail.com>
2023-05-06 21:58:51 +0700

    update village small layout

---
+++ lib/app/modules/villagehosty/views/village_layout_small.dart
@@ -0,0 +1,171 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../../../responsive.dart';
+import '../../../data/models/summary_chart.dart';
+import '../../../data/models/summary_info.dart';
+import '../../../data/models/village_statistics_data.dart';
+import '../../../routes/app_pages.dart';
+import '../../../shared/constant.dart';
+import '../../../shared/custom_text.dart';
+import '../../../shared/info_card.dart';
+import '../../../shared/main_chart.dart';
+import '../../../shared/utils.dart';
+
+class VillageLayoutSmall extends StatelessWidget {
+  VillageLayoutSmall({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: const [
+            Expanded(
+              child: CustomText(
+                text: "จังหวัด: กรุงเทพมหานคร",
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: defaultPadding / 2),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Spacer(flex: 2),
+            ElevatedButton.icon(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(
+                    vertical: defaultPadding, horizontal: defaultPadding / 2),
+              ),
+              icon: const Icon(
+                Icons.insert_drive_file_sharp,
+                size: 16,
+              ),
+              label: const CustomText(
+                text: "รายงาน",
+                color: Colors.white,
+                scale: 0.9,
+              ),
+            ),
+            const SizedBox(width: defaultPadding / 2),
+            ElevatedButton.icon(
+              icon: const Icon(
+                Icons.add_sharp,
+                size: 16,
+              ),
+              label: const CustomText(
+                text: "เพิ่ม/แก้ไข",
+                color: Colors.white,
+                scale: 0.9,
+              ),
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(
+                    vertical: defaultPadding, horizontal: defaultPadding / 2),
+              ),
+              onPressed: () {
+                Get.toNamed(Routes.MANAGE_VILLAGE);
+              },
+            ),
+          ],
+        ),
+        const SizedBox(height: defaultPadding / 2),
+        InfoCard(
+          crossAxisCount: Responsive.isSmallScreen(context) ? 2 : 4,
+          childAspectRatio: 2.0,
+          textScale: 1.0,
+          listSummaryInfo: listVillageSummaryInfo,
+        ),
+        const SizedBox(height: defaultPadding / 2),
+        Container(
+          padding: const EdgeInsets.all(defaultPadding / 2),
+          decoration: BoxDecoration(
+            color: canvasColor,
+            borderRadius: BorderRadius.circular(defaultPadding),
+          ),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Row(
+                  children: const [
+                    CustomText(
+                      text: "ข้อมูลสถิติรายจังหวัด",
+                      weight: FontWeight.bold,
+                    ),
+                  ],
+                ),
+                accentDivider,
+                ListView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: listVillageStatisticsData.length,
+                  itemBuilder: (context, index) {
+                    return VillageStatisticsSmallRow(
+                        index, listVillageStatisticsData[index]);
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+        const SizedBox(height: defaultPadding / 2),
+        MainChart(
+          header: "สถิติข้อมูลเครือข่าย ศส.ปชต.",
+          subHeader: "ระดับจังหวัด",
+          listSummaryChart: summaryVillageChart,
+        ),
+      ],
+    );
+  }
+}
+
+Widget VillageStatisticsSmallRow(
+  int index,
+  VillageStatisticsData villageStatisticsData,
+) {
+  return Row(
+    children: [
+      // Image.network(
+      //   "assets/images/seal/${provinceSummary.seal}",
+      //   height: 38,
+      // ),
+      Expanded(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: defaultPadding / 2),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(defaultPadding),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CustomText(
+                text: "ชื่อหมู่บ้าน : ${villageStatisticsData.name} ",
+                scale: 0.9,
+              ),
+              CustomText(
+                text: "หมู่ที่/บ้านเลขที่ : ${villageStatisticsData.no}",
+                scale: 0.9,
+              ),
+              CustomText(
+                text: "จังหวัด/อำเภอ/ตำบล : ${villageStatisticsData.address}",
+                scale: 0.9,
+                maxLine: 2,
+              ),
+              CustomText(
+                text:
+                    "จำนวนครัวเรือน : ${formatterItem.format(villageStatisticsData.total)}",
+                scale: 0.9,
+              ),
+              const SizedBox(height: defaultPadding / 4),
+              accentDivider,
+            ],
+          ),
+        ),
+      ),
+    ],
+  );
+}

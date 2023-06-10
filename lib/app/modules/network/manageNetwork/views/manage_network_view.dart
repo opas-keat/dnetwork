38dbe1712ee7af4bd3a5749130b2421a05e45c1e
opas-keat <opas.keat@gmail.com>
2023-06-10 12:39:25 +0700

    ช่อง ว/ด/ป เกิด ย้ายไปต่อท้ายเลขบัตรประชาชน

--- lib/app/modules/network/manageNetwork/views/manage_network_view.dart
+++ lib/app/modules/network/manageNetwork/views/manage_network_view.dart
@@ -208,28 +208,6 @@ class ManageNetworkView extends StatelessWidget {
                           ),
                           const SizedBox(height: defaultPadding),
                           CustomText(
-                            text: "ว/ด/ป เกิด",
-                            color: Colors.black87.withOpacity(.9),
-                          ),
-                          const SizedBox(height: defaultPadding / 2),
-                          TextFormField(
-                            keyboardType: TextInputType.text,
-                            decoration: InputDecoration(
-                              fillColor: Colors.white.withOpacity(.8),
-                              filled: true,
-                              border: OutlineInputBorder(
-                                borderRadius:
-                                    BorderRadius.circular(defaultPadding / 2),
-                                borderSide: const BorderSide(
-                                    color: Colors.black54, width: 1),
-                              ),
-                              isCollapsed: true,
-                              contentPadding:
-                                  const EdgeInsets.fromLTRB(12, 14, 12, 12),
-                            ),
-                          ),
-                          const SizedBox(height: defaultPadding),
-                          CustomText(
                             text: "ชื่อ",
                             color: Colors.black87.withOpacity(.9),
                           ),
@@ -275,6 +253,28 @@ class ManageNetworkView extends StatelessWidget {
                           const SizedBox(height: defaultPadding),
                           CustomText(
                             text: "เลขที่บัตรประชาชน",
+                            color: Colors.black87.withOpacity(.9),
+                          ),
+                          const SizedBox(height: defaultPadding / 2),
+                          TextFormField(
+                            keyboardType: TextInputType.text,
+                            decoration: InputDecoration(
+                              fillColor: Colors.white.withOpacity(.8),
+                              filled: true,
+                              border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.circular(defaultPadding / 2),
+                                borderSide: const BorderSide(
+                                    color: Colors.black54, width: 1),
+                              ),
+                              isCollapsed: true,
+                              contentPadding:
+                                  const EdgeInsets.fromLTRB(12, 14, 12, 12),
+                            ),
+                          ),
+                          const SizedBox(height: defaultPadding),
+                          CustomText(
+                            text: "ว/ด/ป เกิด",
                             color: Colors.black87.withOpacity(.9),
                           ),
                           const SizedBox(height: defaultPadding / 2),

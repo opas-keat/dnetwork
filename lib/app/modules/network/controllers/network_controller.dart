29cf040418423febd8833b24b0ddaae7f611f32f
opas-keat <opas.keat@gmail.com>
2023-06-11 15:03:51 +0700

    ภาคีเครือข่าย จัดเรียงข้อมูลมาก-น้อย หรือ ก-ฮ

--- lib/app/modules/network/controllers/network_controller.dart
+++ lib/app/modules/network/controllers/network_controller.dart
@@ -1,11 +1,74 @@
 import 'package:get/get.dart';
 
+import '../../../../main.dart';
+import '../../../data/models/network_statistics.data.dart';
+import '../../../shared/utils.dart';
+
 class NetworkController extends GetxController {
-  //TODO: Implement NetworkController
+  final logTitle = "NetworkController";
+  RxBool isLoading = true.obs;
+  RxBool isLoadingAddNetwork = true.obs;
 
-  final count = 0.obs;
+  final listNetworkStatistics = [].obs;
+  RxBool sortAscending = true.obs;
+  RxInt sortColumnIndex = 0.obs;
 
+  @override
+  void onInit() {
+    super.onInit();
+    isLoading.value = true;
+    listNetworkStatistics.value = listNetworkStatisticsData;
+    update();
+    getNetwork();
+  }
 
+  getNetwork() async {
+    talker.info('$logTitle:getNetwork:');
+    isLoading.value =
+        await Future.delayed(Duration(seconds: randomValue()), () {
+      return false;
+    });
+    update();
+  }
 
-  void increment() => count.value++;
+  void sort<T>(
+    String field,
+    int columnIndex,
+    bool ascending,
+  ) {
+    talker.info('$logTitle:sort:$field');
+    if (field == "name") {
+      ascending
+          ? listNetworkStatistics.value.sort((a, b) => a.name.compareTo(b.name))
+          : listNetworkStatistics.value
+              .sort((a, b) => b.name.compareTo(a.name));
+    } else if (field == "position") {
+      ascending
+          ? listNetworkStatistics.value
+              .sort((a, b) => a.position.compareTo(b.position))
+          : listNetworkStatistics.value
+              .sort((a, b) => b.position.compareTo(a.position));
+    } else if (field == "commissDate") {
+      ascending
+          ? listNetworkStatistics.value
+              .sort((a, b) => a.commissDate.compareTo(b.commissDate))
+          : listNetworkStatistics.value
+              .sort((a, b) => b.commissDate.compareTo(a.commissDate));
+    } else if (field == "commissLocation") {
+      ascending
+          ? listNetworkStatistics.value
+              .sort((a, b) => a.commissLocation.compareTo(b.commissLocation))
+          : listNetworkStatistics.value
+              .sort((a, b) => b.commissLocation.compareTo(a.commissLocation));
+    } else if (field == "address") {
+      ascending
+          ? listNetworkStatistics.value
+              .sort((a, b) => a.address.compareTo(b.address))
+          : listNetworkStatistics.value
+              .sort((a, b) => b.address.compareTo(a.address));
+    }
+    sortColumnIndex.value = columnIndex;
+    sortAscending.value = ascending;
+    update();
+  }
 }
--- lib/app/modules/network/views/network_layout_large.dart
+++ lib/app/modules/network/views/network_layout_large.dart
@@ -75,7 +75,7 @@ class NetworkLayoutLarge extends StatelessWidget {
                 textScale: 1.4,
               ),
               const SizedBox(height: defaultPadding / 2),
-              const NetworkStatistics(),
+              NetworkStatistics(),
             ],
           ),
         ),
